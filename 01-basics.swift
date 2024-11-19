1a6092631dfd9de40345d9b39112b37fbcb744e4
Ayush Srivastava <srivastavaayush6393@gmail.com>
2024-11-19 17:46:10 +0530

    Got to know about basics of swift

---
+++ 01-basics.swift
@@ -0,0 +1,19 @@
+
+func basics_01(){
+    
+//    The naming convention in swift will be camelCase
+    
+    /*
+     keywords in swift which are used to declare variables
+     */
+//   if we declare something using let than it's mean we can't update the value of that varaible basically they are immutable
+    
+    let name:String="Ayush Srivastava"
+    print(name)
+    
+//  if we declare something in var than we can reintialize it's value as well
+    var userName:String="Aaayushhh"
+    userName="Ayush"
+    print(userName)
+}
+
---
+++ main.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/*
+ This is our main file from this file we will call function of other file to execute them
+ becuse in swift I am using command line tools so from here we can run only 1 file which is our main file.
+ */
+
+basics_01()
