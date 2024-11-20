ba30fd993f8ca826c234d1771b005614dad0b6f4
Ayush Srivastava <srivastavaayush6393@gmail.com>
2024-11-20 21:35:17 +0530

    Got to know about Data-types in swift.

---
+++ 02-dataTypes.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func dataTypes02() {
+    // String
+    let name = "Ayush Srivastava"
+    print("Name: \(name)")
+    
+    // Int
+    let salary: Int = 15000
+    print("Salary: \(salary)")
+    
+    // Boolean
+    let genederIsMale: Bool = true
+    print("Is Male: \(genederIsMale)")
+    
+    // Date
+    let todaysDate: Date = Date()
+    print("Today's Date: \(todaysDate)")
+    
+}
